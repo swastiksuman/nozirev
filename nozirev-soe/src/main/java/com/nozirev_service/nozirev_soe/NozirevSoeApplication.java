56f6ea02fe00940abbba2e083e2e5648fcfc8c4b
Swastik Panda <swastiksuman@gmail.com>
2025-12-30 23:48:09 +0530

    ui and backend project

---
+++ nozirev-soe/src/main/java/com/nozirev_service/nozirev_soe/NozirevSoeApplication.java
@@ -0,0 +1,13 @@
+package com.nozirev_service.nozirev_soe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NozirevSoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NozirevSoeApplication.class, args);
+	}
+
+}
---
+++ nozirev-soe/src/test/java/com/nozirev_service/nozirev_soe/NozirevSoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nozirev_service.nozirev_soe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NozirevSoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
